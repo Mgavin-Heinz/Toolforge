320addc3342e9127689186abd6fa045cbff986d8
Coder-Heinz-Delta <20240263@ctucareer.co.za>
2026-05-06 17:55:48 +0200

    created basic login/sign up/ dashboard/Home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import ToolPage from './pages/ToolPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tool/:id" element={<ToolPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
